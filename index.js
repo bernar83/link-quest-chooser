85b4a1f0f4486dba6cfb2aad90b02aab5a6b5254
Adrian Bernardo <nabernardo93@gmail.com>
2020-03-10 22:53:03 -0700

    Add pdfparser

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const PDFParser = require("pdf2json");
 
+function main() {
+  const pdfParser = new PDFParser();
+  pdfParser.on("pdfParser_dataError", errData =>
+    console.error(errData.parserError)
+  );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    pdfData.formImage.Pages[0].Texts.forEach(text => {
+      console.log(text.R[0]);
+    });
+  });
+
+  pdfParser.loadPDF("./quests/quests_for_question.pdf");
+}
+
+main();
